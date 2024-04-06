e2a440084e5d03356c721059e475c9ba19256311
MarinaV27 <Vasilishinamari@gmail.com>
2024-04-06 17:00:19 +0300

    pixabay alternate then/await

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,18 +1,21 @@
-export function getImages(q) {
-    const BASE_URL = 'https://pixabay.com';
-    const END_POINT = '/api/'
-    const params = new URLSearchParams({
+import axios from "axios";
+
+export async function getImages(query, currentPage) {
+    const BASE_URL = 'https://pixabay.com/api/';
+    const params = {
       key: '43180958-6b06a0884e2ebb29e4f1cb8bf',
-      q,
+      q: query,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: 'true',
-    });
-    const url = `${BASE_URL}${END_POINT}?${params}`;
-    return fetch(url)
-      .then(res => res.json())
-      .catch(error => {
+      per_page: 15,
+      page: currentPage,
+    };
+    try {
+      const res = await axios.get(BASE_URL, {params});
+      return res.data
+    } catch (error) {
         console.error('Error occurred while fetching images:', error);
         throw error;
-      });
+      };
   }

45d5290c419a54518bbc836c431a000ddc62a862
MarinaV27 <149487248+MarinaV27@users.noreply.github.com>
2024-04-06 21:10:20 +0000

    Deploying to gh-pages from @ MarinaV27/goit-js-hw-12@0cb64dae4667fa26fa10c080befab8a716c72153 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,6 +1,6 @@
-import{a as c,i as n,S as f}from"./assets/vendor-09d7c26e.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&s(a)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}})();async function m(i,t){const o="https://pixabay.com/api/",s={key:"43180958-6b06a0884e2ebb29e4f1cb8bf",q:i,image_type:"photo",orientation:"horizontal",safesearch:"true",per_page:15,page:t};try{return(await c.get(o,{params:s})).data}catch(e){throw console.error("Error occurred while fetching images:",e),e}}function u(i){return i.map(({webformatURL:t,largeImageURL:o,tags:s,likes:e,views:r,comments:a,downloads:l})=>`<li class="list-item">
-       <a class="gallery-link" href ="${o}">
-       <img src="${t}" alt="${s}" class="gallery-image"
+import{a as w,i as c,S}from"./assets/vendor-09d7c26e.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const n of o.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function t(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function i(e){if(e.ep)return;e.ep=!0;const o=t(e);fetch(e.href,o)}})();async function l(r,s){const t="https://pixabay.com/api/",i={key:"43180958-6b06a0884e2ebb29e4f1cb8bf",q:r,image_type:"photo",orientation:"horizontal",safesearch:"true",per_page:15,page:s};try{return(await w.get(t,{params:i})).data}catch(e){throw console.error("Error occurred while fetching images:",e),e}}function h(r){return r.map(({webformatURL:s,largeImageURL:t,tags:i,likes:e,views:o,comments:n,downloads:b})=>`<li class="list-item">
+       <a class="gallery-link" href ="${t}">
+       <img src="${s}" alt="${i}" class="gallery-image"
        </a>
        
         <ul class="information-list">
@@ -11,19 +11,19 @@ import{a as c,i as n,S as f}from"./assets/vendor-09d7c26e.js";(function(){const 
           </li>
           <li class="item-information-container">
             <h2 class="main-info"> Views </h2>
-              <p class="info">${r}</p>
+              <p class="info">${o}</p>
             
           </li>
           <li class="item-information-container">
             <h2 class="main-info"> Comments </h2>
-              <p class="info">${a}</p>
+              <p class="info">${n}</p>
             
           </li>
           <li class="item-information-container">
             <h2 class="main-info"> Downloads </h2>
-              <p class="info">${l}</p>
+              <p class="info">${b}</p>
           </li>
         </ul>
      
-      </li>`).join("")}const g=document.querySelector(".search-form"),d=document.querySelector(".list"),h=document.querySelector(".loader");document.querySelector(".btn-load-more");g.addEventListener("submit",p);async function p(i){i.preventDefault(),h.classList.add("loader");const t=i.target.elements.search.value.trim();if(t===""){n.error({backgroundColor:"lightred",icon:!1,progressBar:!1,close:!1,position:"topRight",message:"Please, fill the field!"});return}try{const o=await m(t);o.hits&&o.hits.length===0&&n.error({backgroundColor:"red",progressBar:!1,close:!1,position:"topRight",message:"Sorry, there are no images matching your search query. Please try again!"});const s=u(o.hits);d.innerHTML=s,new f(".gallery-link",{captionsData:"alt"}).refresh()}catch(o){console.error("Error occurred while fetching images:",o),n.error({backgroundColor:"red",icon:!1,progressBar:!1,close:!1,position:"topRight",message:"Sorry, an error occurred while fetching images. Please try again!"})}}
+      </li>`).join("")}let g,a=1,d=0;const v=15,P=document.querySelector(".search-form"),m=document.querySelector(".list"),p=document.querySelector(".loader"),u=document.querySelector(".btn-load-more");P.addEventListener("submit",q);async function q(r){r.preventDefault();const s=r.target.elements.search.value.trim();if(m.innerHTML="",a=1,f(),await l(g,a),s===""){c.error({backgroundColor:"lightred",icon:!1,progressBar:!1,close:!1,position:"topRight",message:"Please, fill the field!"});return}try{const t=await l(s);f(),t.hits&&t.hits.length===0&&c.error({backgroundColor:"red",progressBar:!1,close:!1,position:"topRight",message:"Sorry, there are no images matching your search query. Please try again!"});const i=h(t.hits);m.innerHTML=i,new S(".gallery-link",{captionsData:"alt",captionDelay:250}).refresh()}catch(t){console.error("Error occurred while fetching images:",t),c.error({backgroundColor:"red",icon:!1,progressBar:!1,close:!1,position:"topRight",message:"Sorry, an error occurred while fetching images. Please try again!"}),L()}}u.addEventListener("click",M);d=Math.ceil(data.totalResult/v);async function M(){a+=1,f();try{const r=await l(g,a);h(r),a>=d&&(y(),c.show({color:"green",message:"Sorry, you have reached the end of collection.",position:"topCenter",timeout:1e3}))}catch(r){console.log(r)}L(),k()}function $(){u.classList.remove("hidden")}function y(){u.classList.add("hidden")}function f(){p.classList.remove("hidden")}function L(){p.classList.add("hidden")}function k(){a>=d?y():$()}
 //# sourceMappingURL=commonHelpers.js.map
